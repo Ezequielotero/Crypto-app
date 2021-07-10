9262d54448fb5fe29114a97410753271d5b67e4c
Ezequielotero <75461873+Ezequielotero@users.noreply.github.com>
2021-07-10 13:58:55 -0300

    detail changes

--- client/football/src/Components/Detail/Detail.jsx
+++ client/football/src/Components/Detail/Detail.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {useSelector } from'react-redux'
 import Loading from '../Loading/Loading'
 import Grow from '@material-ui/core/Grow';
+import './Detail.css'
 const Detail = () => {
     const detail = useSelector(state => state.detail)
     const [checked, setChecked]=useState('')
@@ -12,16 +13,22 @@ const Detail = () => {
         setChecked(e.target.value)
     }
     return (
-        <div>
+        <div className='detail-div'>
              
-                 <div>
-                <h2>{detail[0]?.name}</h2>
+                 <div className='detail-name' >
+                <h3>{detail[0]?.name}</h3>
                  </div>
-                 <div>
+                 <div className='detail-realising'>
+                 <h2>Realising date</h2>
                 <h2>{detail[0]?.released}</h2>
+                 <div className='filters-box2'>
+                <img className='detail-img' src={detail[0]?.background} alt="" />
+                 </div>
                  </div>
                  <div>
                  <p>{detail[0]?.description}</p>
+                 <p>{detail[0]?.platforms.join(', ')}</p>
+                 <p>{detail[0]?.genres.join(', ')}</p>
                  </div>
                 
                 <button onClick={handleClick} value='checked'>ok</button>
